3512d0162f40fbde6dbd607ddb568cecd48e5a32
Peter Heslin <p.j.heslin@dur.ac.uk>
2007-08-06 23:06:12 +0100

    3.0.10
    darcs-hash:20070806220612-a4462-eb26cfc22a0ffc1932a4f15c0b4608a9a43da295

--- Base.pm
+++ Base.pm
@@ -27,7 +27,7 @@
 package Diogenes::Base;
 require 5.006;
 
-$Diogenes::Base::Version =  "3.0.9";
+$Diogenes::Base::Version =  "3.0.10";
 $Diogenes::Base::my_address = 'p.j.heslin@durham.ac.uk';
 
 use strict;
