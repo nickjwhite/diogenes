f86bb551f5cdd233761cd7a81b74e3e5ba99ec65
Nick White <git@njw.name>
2015-03-09 22:46:40 +0000

    Fix index_lsj_trans.pl to find all translation tags
    2 bugs in finding translation tags have been fixed:
    - Wouldn't match anything from the new XML files due to reliance on
      the old single-line format.
    - Would only match one translation per entry.

--- utils/index_lsj_trans.pl
+++ utils/index_lsj_trans.pl
@@ -4,13 +4,10 @@ use strict;
 my $i = 0;
 
 while (<>) {
-    if (m/<entryFree[^>]*key\s*=\s*\"([^"]*)\"/)
-    {
-        if (m/<tr\s[^>]*>(.*?)<\/tr>/) {
-            my $trans = $1;
-            $trans =~ s/[,;:].*$//;
-            print "$i $trans\n";
-        }
+    if (/<tr\s[^>]*>(.*?)<\/tr>/) {
+        my $trans = $1;
+        $trans =~ s/[,;:].*$//;
+        print "$i $trans\n";
     }
     $i += length $_;
 }
