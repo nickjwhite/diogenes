b9f9975ec8ac9bf38a219b31a1e2a91681991526
Peter Heslin <pheslin@gmail.com>
2016-12-09 23:56:40 +0000

    Code to write db settings to file

--- dist/nwjs/dbPopup.js
+++ dist/nwjs/dbPopup.js
@@ -1,3 +1,4 @@
+var gui = require('nw.gui');
 var fs = require('fs');
 var path = require('path');
 
@@ -7,27 +8,27 @@ var settingsPath = gui.App.dataPath;
 var settingsDir = path.join(settingsPath, 'default');
 var settingsFile = path.join(settingsDir, 'diogenes.prefs');
 
-if (! fs.existsSync(settingsDir)) {
-    fs.mkdir(path, function (e) {
-        if (e) throw e;
-        console.log("Created directory " + path);
+function setPath(dbName, folderPath) {
+    fs.readFile(settingsFile, (err, data) => {
+        if (err) throw err;
+        var dir = dbName.toLowerCase() + '_dir';
+        var newLine = dir + ' "' + folderPath + '"';
+        var re = new RegExp('^'+dir+'\s+"?(.*)"?$', 'm');
+        var newData;
+        if (re.test(data)) {
+            newData = data.replace(re, newLine);
+        }
+        else {
+            newData = data + "\n" + newLine;
+        }
+        fs.writeFile(settingsFile, newData, (err) => {
+            if (err) throw err;
+        });
     });
+    showPath(dbName, folderPath);
 }
 
-fs.readFile(SettingsFile, (err, data) => {
-    if (!err) {
-        console.log(data);
-    }
-});
-
-var contents = fs.readFileSync(lockFile, {encoding: 'ascii'});
-    var rePid  = /^pid (.*)$/m;
-    var rePort = /^port (.*)$/m;
-    var ar = rePid.exec(contents);
-    var pid = ar[1];
-
-
-function setPath (dbName, folderPath) {
+function showPath (dbName, folderPath) {
     var showPath = document.querySelector('#'+dbName+'path');
     showPath.innerHTML = folderPath;
 }
@@ -48,22 +49,43 @@ function bindClickEvent (dbName) {
     });
 }
 
-function saveSettings() {
-
-}
-
 window.onload = function () {
+    // Set up click events
     bindClickEvent('PHI');
     bindClickEvent('TLG');
     bindClickEvent('DDP');
-    var button = document.querySelector('#save');
-    button.addEventListener('click', function () {
-        saveSettings();
-        window.close()
-    });
-    button = document.querySelector('#cancel');
+    button = document.querySelector('#close');
     button.addEventListener('click', function () {
         window.close()
     });
+
+    // Create settings dir, if necessary
+    if (! fs.existsSync(settingsDir)) {
+        fs.mkdir(path, function (e) {
+            if (e) throw e;
+            console.log("Created directory " + path);
+        });
+    }
+    // Read existing db settings
+    fs.readFile(settingsFile, (err, data) => {
+        if (!err) {
+            console.log("Reading " + settingsFile);
+            console.log(data);
+            var reTLG = /^tlg_dir\s+"?(.*)"?$/m;
+            var rePHI = /^phi_dir\s+"?(.*)"?$/m;
+            var reDDP = /^ddp_dir\s+"?(.*)"?$/m;
+            var ar;
+            ar = reTLG.exec(data);
+            showPath('TLG', ar[1]);
+            ar = rePHI.exec(data);
+            showPath('PHI', ar[1]);
+            ar = reDDP.exec(data);
+            showPath('DDP', ar[1]);
+        }
+});
+
+
+
+    
 };
 
