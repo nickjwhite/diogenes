83a76026e27429405782f9be91e95f5cbf2b9b7b
Nick White <git@njw.name>
2018-09-17 17:39:03 +0100

    Remove TODO item about triggering diogenes earlier, no need and it would add complexity

--- dist/electron/main.js
+++ dist/electron/main.js
@@ -4,8 +4,6 @@ const path = require('path')
 const process = require('process')
 const fs = require('fs')
 
-// TODO: probably can trigger diogenes start earlier, before electron's app.ready
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
