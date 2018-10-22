edd69f9233059c9cd7077503303e496d1ef71d1d
Nick White <git@njw.name>
2018-10-22 16:52:13 +0100

    Simplify firstrun.js significantly, and regularise js usage within it

--- dist/electron/pages/firstrun.js
+++ dist/electron/pages/firstrun.js
@@ -10,25 +10,24 @@ function setPath(dbName, folderPath) {
 	// check if folderPath is defined.
 	let data = window.dioReadSettings()
 	if(data === null) {
-		console.log('No prefs file found at ' + window.dioSettingsFile);
-		data = '# Created by electron';
+		data = '# Created by electron'
 	}
-	var dir = dbName.toLowerCase() + '_dir';
-	var newLine = dir + ' "' + folderPath + '"';
-	var re = new RegExp('^'+dir+'.*$', 'm');
-	var newData;
+	let db_l = dbName.toLowerCase()
+	let newLine = `${db_l}_dir "${folderPath}"`
+	let re = new RegExp(`^${db_l}_dir.*$`, 'm')
+	let newData
 	if(re.test(data)) {
-		newData = data.replace(re, newLine);
+		newData = data.replace(re, newLine)
 	} else {
-		newData = data + "\n" + newLine;
+		newData = `${data}\n${newLine}`
 	}
 	window.dioWriteSettings(newData)
-	showPath(dbName, folderPath);
-	readyDoneButton();
+	showPath(dbName, folderPath)
+	readyDoneButton()
 }
 
 function showPath(dbName, folderPath) {
-	document.getElementById(`${dbName}path`).innerHTML = folderPath;
+	document.getElementById(`${dbName}path`).innerHTML = folderPath
 
 	checkmark = document.getElementById(`${dbName}ok`)
 	if(window.dioExistsSync(`${folderPath}/authtab.dir`) || window.dioExistsSync(`${folderPath}/AUTHTAB.DIR`)) {
@@ -36,7 +35,7 @@ function showPath(dbName, folderPath) {
 		checkmark.classList.remove('warn')
 		checkmark.classList.add('valid')
 	} else {
-		checkmark.innerHTML = '✕ No authtab.dir found; this may not be a valid database location'
+		checkmark.innerHTML = '✕ No authtab.dir found; this doesn\'t look like a correct database location'
 		checkmark.classList.remove('valid')
 		checkmark.classList.add('warn')
 	}
@@ -62,46 +61,40 @@ function bindClickEvent(dbName) {
 			title: `Set ${dbName} location`,
 			properties: ['openDirectory']
 		}))
-	});
+	})
 }
 
-function dbSettingsSetup() {
-	// Set up click events
-	bindClickEvent('PHI');
-	bindClickEvent('TLG');
-	bindClickEvent('DDP');
+function firstrunSetup() {
+	// Create settings dir, if necessary
+	if(!window.dioExistsSync(window.dioSettingsDir)) {
+		window.dioMkSettingsDir()
+	}
+
+	readyDoneButton()
 
 	document.getElementById('done').addEventListener('click', () => {
 		window.location.href = `http://localhost:${window.dioPort}`
-	});
+	})
 
-	// Create settings dir, if necessary
-	if(!window.dioExistsSync(window.dioSettingsDir)) {
-		window.dioMkSettingsDir
-	}
 	// Read existing db settings
 	let data = window.dioReadSettings()
-	if(data === null) {
-		return
-	}
-	var reTLG = /^tlg_dir\s+"?(.*?)"?$/m;
-	var rePHI = /^phi_dir\s+"?(.*?)"?$/m;
-	var reDDP = /^ddp_dir\s+"?(.*?)"?$/m;
-	var ar;
-	ar = reTLG.exec(data);
-	if(ar) {
-		showPath('TLG', ar[1]);
-	}
-	ar = rePHI.exec(data);
-	if(ar) {
-		showPath('PHI', ar[1]);
-	}
-	ar = reDDP.exec(data);
-	if(ar) {
-		showPath('DDP', ar[1]);
-	}
 
-	readyDoneButton();
-};
+	for(let i = 0; i < dbs.length; i++) {
+		let db = dbs[i]
 
-window.addEventListener('load', dbSettingsSetup, false);
+		bindClickEvent(db)
+
+		if(data === null) {
+			continue
+		}
+
+		let db_l = db.toLowerCase()
+		let re = `/^${db_l}_dir\s+"?(.*?)"?$/m;/`
+		let ar = re.exec(data)
+		if(ar) {
+			showPath(db, ar[1])
+		}
+	}
+}
+
+window.addEventListener('load', firstrunSetup, false)
