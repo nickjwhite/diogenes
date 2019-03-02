5de0cc0a3778184b7ab62c0da22c1b86da622dba
Peter Heslin <pheslin@gmail.com>
2019-03-02 19:04:06 +0000

    Make lsj-head and lsj-trans compatible with Logeion LSJ

--- utils/index_lsj_head.pl
+++ utils/index_lsj_head.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
+binmode(STDIN);
+binmode(STDERR, ":utf8");
 
 my $i = 0;
 
 while (<>) {
-    if (m/<entryFree[^>]*key\s*=\s*\"(.*?)\"/)
+    if (m/<(?:entryFree|div2)[^>]*key\s*=\s*\"(.*?)\"/)
     {
         my $key = $1;
         $key =~ s/[^a-z]//g;
--- utils/index_lsj_trans.pl
+++ utils/index_lsj_trans.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/perl -w
 use strict;
+binmode(STDIN);
+binmode(STDERR, ":utf8");
 
 my $i = 0;
 
 while (<>) {
-    if (m/<entryFree[^>]*key\s*=\s*\"(.*?)\"/)
+    if (m/<(?:entryFree|div2)[^>]*key\s*=\s*\"(.*?)\"/)
     {
-        if (m/<tr\s[^>]*>(.*?)<\/tr>/) {
+        if (m/<tr\s[^>]*>(.*?)<\/tr>/ or
+            m/<sense\s[^>]*>\s*<i>(.*?)<\/i>/) {
             my $trans = $1;
             $trans =~ s/[,;:].*$//;
             print "$i $trans\n";
